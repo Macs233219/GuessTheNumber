ea41e963a1a3894e87db0450ac22c08188222452
Dooblop <marlon.canche233219@potros.itson.edu.mx>
2025-01-30 19:15:41 -0700

    Front de Guess The Number
    Parte de front de Guess The Number

---
+++ app/src/main/java/itson/edu/mx/guessthenumber/MainActivity.kt
@@ -0,0 +1,37 @@
+package itson.edu.mx.guessthenumber
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    var minValue = 0;
+    var maxValue = 100;
+    var num:Int = 0;
+    var won = false;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val guessings: TextView = findViewById(R.id.guessings)
+        val down: Button = findViewById(R.id.down)
+        val up: Button = findViewById(R.id.up)
+        val generate: Button = findViewById(R.id.generate)
+        val guessed: Button = findViewById(R.id.guessed)
+
+        generate.setOnClickListener {
+            num = Random.nextInt(minValue, maxValue)
+            guessings.setText(num.toString())
+            generate.visibility = View.INVISIBLE
+            guessed.visibility = View.VISIBLE
+        }
+        
+    }
+}
